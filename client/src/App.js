bd678aafe8bf45f1e7452a768e3f73dcb0b3b6fc
Rajan66 <rajanmaharjan042@gmail.com>
2023-12-08 16:22:13 +0545

    Setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
