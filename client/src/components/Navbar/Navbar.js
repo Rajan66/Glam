09333341ca29199b1968ced7249025480924ab4b
Lumana2002 <lumana.bajracharya@gmail.com>
2023-12-08 23:53:03 +0545

    nav ali ali hehe

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import "./styles.css"
 
 const Navbar = () => {
+const [click, setClick] = useState(false)
+const handleClick = () => setClick(!click)
   return (
-    <div>This is nav bar</div>
+    <div className= 'header'>
+      <nav className='navbar'>
+        <a href='#' className='logo'>
+          {/* <img rakhne hoki logo ko vanera banako :lumanathink: */ }
+        </a>
+        <div className='ham' onClick={handleClick}>
+          {click ? (<FaTimes size={30} style={{color: '#fff'}}/>)
+          : (<FaBars size={30} style={{color: '#fff'}}/>) }
+          
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className='nav-item'>
+            <a href='#'>Home</a>
+          </li>
+          <li className='nav-item'>
+            <a href='#'>About</a>
+          </li>
+          <li className='nav-item'>
+            <a href='#'>Contact</a>
+          </li>
+          <li className='nav-item'>
+            <a href='#'>Demo</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
