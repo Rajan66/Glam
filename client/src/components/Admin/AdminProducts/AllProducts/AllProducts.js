ba247b63bff912cbcc424ded1d9c7f1feab9309b
Rajan66 <rajanmaharjan042@gmail.com>
2024-01-06 15:48:56 +0545

    Removed unused codes

--- client/src/components/Admin/AdminProducts/AllProducts/AllProducts.js
+++ client/src/components/Admin/AdminProducts/AllProducts/AllProducts.js
@@ -3,7 +3,7 @@ import './AllProducts.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { deleteProduct } from '../../../../actions/products'
 
-import { Grid, IconButton } from '@mui/material'
+import { IconButton } from '@mui/material'
 import DeleteIcon from "@mui/icons-material/Delete"
 import UpdateIcon from "@mui/icons-material/Edit"
 
@@ -12,61 +12,39 @@ const AllProducts = ({ setCurrentId }) => {
     const dispatch = useDispatch()
     const products = useSelector((state) => state.products)
     return (
-        <>
-            {/* <Grid>
-                {products.map((product) => (
-                    <Grid key={product._id} item>
-                        <div style={{ display: "flex", padding: "20px" }}> */}
-            {/* <img src={dummyImg} width="100%" height="100%" /> */}
-            {/* <p>{product.title}</p>
-                            <p><span>{product.tags}</span></p>
-                            <p>{product.price}</p>
-                            <IconButton color='primary' onClick={() => setCurrentId(product._id)} >
-                                <UpdateIcon />
-                            </IconButton>
-                            <IconButton color='primary' onClick={() => dispatch(deleteProduct(product._id))} >
-                                <DeleteIcon />
-                            </IconButton>
-                        </div>
-                    </Grid>
-                ))}
-
-            </Grid > */}
-
-            <div className='container'>
-                <table>
-                    <thead>
-                        <tr className='tb' >
-                            <th>Image</th>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Category</th>
-                            <th>Price</th>
-                            <th>Action</th>
+        <div className='container'>
+            <table>
+                <thead>
+                    <tr className='tb' >
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                        <th>Price</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product) => (
+                        <tr className='tb'>
+                            <td><div><img src={product.productImage} /></div></td>
+                            <td><div><span>{product.title}</span></div></td>
+                            <td>{product.title}</td>
+                            <td>{product.category}</td>
+                            <td>2000</td>
+                            <td>
+                                <IconButton color='primary' onClick={() => setCurrentId(product._id)} >
+                                    <UpdateIcon />
+                                </IconButton>
+                                <IconButton color='primary' onClick={() => dispatch(deleteProduct(product._id))} >
+                                    <DeleteIcon />
+                                </IconButton>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        {products.map((product) => (
-                            <tr className='tb'>
-                                <td><div><img src={product.productImage} /></div></td>
-                                <td><div><span>{product.title}</span></div></td>
-                                <td>{product.title}</td>
-                                <td>{product.category}</td>
-                                <td>2000</td>
-                                <td>
-                                    <IconButton color='primary' onClick={() => setCurrentId(product._id)} >
-                                        <UpdateIcon />
-                                    </IconButton>
-                                    <IconButton color='primary' onClick={() => dispatch(deleteProduct(product._id))} >
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
