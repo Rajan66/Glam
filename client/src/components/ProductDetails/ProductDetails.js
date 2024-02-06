66e05cc23d7d1e9c877acd0954371ddba51c546a
Lumana2002 <lumana.bajracharya@gmail.com>
2024-02-06 14:49:51 +0545

    remove color and size

--- client/src/components/ProductDetails/ProductDetails.js
+++ client/src/components/ProductDetails/ProductDetails.js
@@ -115,42 +115,8 @@ const ProductDetails = () => {
                             </p>
                             <p class="text-green-600 dark:text-green-300 ">7 in stock</p>
                         </div>
-                        <div class="flex items-center mb-8">
-                            <h2 class="w-16 mr-6 text-xl font-bold dark:text-gray-400">
-                                Colors:</h2>
-                            <div class="flex flex-wrap -mx-2 -mb-2">
-                                <button
-                                    class="p-1 mb-2 mr-2 border border-transparent hover:border-blue-400 dark:border-gray-800 dark:hover:border-gray-400 ">
-                                    <div class="w-6 h-6 bg-cyan-300"></div>
-                                </button>
-                                <button
-                                    class="p-1 mb-2 mr-2 border border-transparent hover:border-blue-400 dark:border-gray-800 dark:hover:border-gray-400">
-                                    <div class="w-6 h-6 bg-green-300 "></div>
-                                </button>
-                                <button
-                                    class="p-1 mb-2 border border-transparent hover:border-blue-400 dark:border-gray-800 dark:hover:border-gray-400">
-                                    <div class="w-6 h-6 bg-red-200 "></div>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="flex items-center mb-8">
-                            <h2 class="w-16 text-xl font-bold dark:text-gray-400">
-                                Size:</h2>
-                            <div class="flex flex-wrap -mx-2 -mb-2">
-                                <button
-                                    class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 dark:border-gray-400 hover:text-blue-600 dark:hover:border-gray-300 dark:text-gray-400">XL
-                                </button>
-                                <button
-                                    class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 hover:text-blue-600 dark:border-gray-400 dark:hover:border-gray-300 dark:text-gray-400">S
-                                </button>
-                                <button
-                                    class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 hover:text-blue-600 dark:border-gray-400 dark:hover:border-gray-300 dark:text-gray-400">M
-                                </button>
-                                <button
-                                    class="py-1 mb-2 mr-1 border w-11 hover:border-blue-400 hover:text-blue-600 dark:border-gray-400 dark:hover:border-gray-300 dark:text-gray-400">XS
-                                </button>
-                            </div>
-                        </div>
+                        
+                        
                         <div class="w-32 mb-8 ">
                             <label for=""
                                 class="w-full text-xl font-semibold text-gray-700 dark:text-gray-400">Quantity</label>
