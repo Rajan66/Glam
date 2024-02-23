fc7e7ca2ac485d8ac881926c3904c071f4fb328b
Lumana2002 <lumana.bajracharya@gmail.com>
2024-02-23 22:04:42 +0545

    bg color

--- client/src/components/ProductDetails/ProductDetails.js
+++ client/src/components/ProductDetails/ProductDetails.js
@@ -12,7 +12,7 @@ const ProductDetails = () => {
     return (
         <>
         <Navbar/>
-        <section class="overflow-hidden bg-grey py-11 font-poppins dark:bg-gray-800">
+        <section class="overflow-hidden bg-gray py-11 font-poppins dark:bg-gray-800">
             <div class="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
                 <div class="flex flex-wrap -mx-4">
                     <div class="w-full px-4 md:w-1/2 ">
